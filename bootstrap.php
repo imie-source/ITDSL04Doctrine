4ac92c239f3a23b25e24daa7fb899f6a97becfc3
Gabriel Block <gabriel.block@imie.fr>
2017-01-24 10:40:56 +0100

    Doctrine configuration

--- bootstrap.php
+++ bootstrap.php
@@ -1,2 +1,15 @@
 <?php
 // Doctrine configuration
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+$isDevMode = true;
+$config = Setup::createAnnotationMetadataConfiguration(array(ENTITY_DIR), $isDevMode);
+
+// database configuration parameters
+$conn = [
+    'driver' => 'pdo_sqlite',
+    'path' => DATABASE_FILE,
+];
+
+$em = EntityManager::create($conn, $config);
--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
 
+define("ENTITY_DIR", "src/Entity");
+define("DATABASE_FILE", __DIR__ . '/db.sqlite');
+
 require_once "vendor/autoload.php";
+require_once "bootstrap.php";
+
 
