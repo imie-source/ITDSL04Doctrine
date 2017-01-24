b1fcb143d669c062b869fd4d4a2655b634bace75
Gabriel Block <gabriel.block@imie.fr>
2017-01-24 14:22:54 +0100

    Doctrine persist example

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,10 +2,23 @@
 
 namespace Imie\Controller;
 
+use \Imie\Entity\Product;
+
 class TestController extends Controller{
 
     public function indexAction(){
-        
+
+        $em = $this->getDoctrine();
+
+        $product = new Product();
+        $product->setName("Android 7");
+
+        $em->persist($product);
+
+        $em->flush();
+
+        echo "Nouveau produit inséré :";
+        var_dump($product);      
     }
 
 }
