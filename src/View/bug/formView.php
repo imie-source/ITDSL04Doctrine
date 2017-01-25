11087587f895f9d4157390c9bb52a6f5e0d03c2b
Gabriel Block <gabriel.block@imie.fr>
2017-01-25 14:06:05 +0100

    Better form (bootstrap)

--- src/View/bug/formView.php
+++ src/View/bug/formView.php
@@ -2,33 +2,41 @@
 <a href="<?= PATH ?>/index.php/bug/index">Retour à la liste</a>
 <form method="POST">
     <input type="hidden" name="id" value="<?= $bug->getId()?>" />
-    <label>
-        Description :
-        <textarea name="description"><?= $bug->getDescription() ?></textarea>
-    </label>
-    <label>
-        Déclarant :
-        <select name="reporter">
-            <?php foreach($users as $user): ?>
-                <option value="<?= $user->getId() ?>"><?= $user->getName() ?></option>
-            <?php endforeach; ?>
-        </select>
-    </label>
-    <label>
-        Ingénieur affecté :
-        <select name="engineer">
-            <?php foreach($users as $user): ?>
-                <option value="<?= $user->getId() ?>"><?= $user->getName() ?></option>
-            <?php endforeach; ?>
-        </select>
-    </label>
-    <label>
-        Produits :
-        <select name="products[]" multiple>
-            <?php foreach($products as $product): ?>
-                <option value="<?= $product->getId() ?>"><?= $product->getName() ?></option>
-            <?php endforeach; ?>
-        </select>
-    </label>
-    <input type="submit" />
+    <div class="form-group">
+        <label>
+            Description :
+            <textarea name="description" class="form-control"><?= $bug->getDescription() ?></textarea>
+        </label>
+    </div>
+    <div class="form-group">
+        <label>
+            Déclarant :
+            <select name="reporter" class="form-control">
+                <?php foreach($users as $user): ?>
+                    <option value="<?= $user->getId() ?>"><?= $user->getName() ?></option>
+                <?php endforeach; ?>
+            </select>
+        </label>
+    </div>
+    <div class="form-group">
+        <label>
+            Ingénieur affecté :
+            <select name="engineer" class="form-control">
+                <?php foreach($users as $user): ?>
+                    <option value="<?= $user->getId() ?>"><?= $user->getName() ?></option>
+                <?php endforeach; ?>
+            </select>
+        </label>
+    </div>
+    <div class="form-group">
+        <label>
+            Produits :
+            <select name="products[]" multiple class="form-control">
+                <?php foreach($products as $product): ?>
+                    <option value="<?= $product->getId() ?>"><?= $product->getName() ?></option>
+                <?php endforeach; ?>
+            </select>
+        </label>
+    </div>
+    <input type="submit" class="btn btn-primary"/>
 </form>
