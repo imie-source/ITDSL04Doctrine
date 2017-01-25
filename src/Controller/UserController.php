b90632e006d13306541d104de76bd7e64e30d189
Gabriel Block <gabriel.block@imie.fr>
2017-01-25 12:04:02 +0100

    Remove user action

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -18,6 +18,7 @@ class UserController extends Controller{
     }
 
     // Add form & submission
+    // This action is also used for update
     public function addAction($args){
         $user = new User();
         $em = $this->getDoctrine();
@@ -29,12 +30,13 @@ class UserController extends Controller{
 
             if(is_null($user)){
                 header('Location: ' . PATH . '/index.php');
+                return;
             }
         }
         
         // Check if we come from a form submission
         if(isset($_POST['name'])){
-            // new User object
+            // set user attributes
             $user->setName(strip_tags($_POST['name']));
             // Tell Doctrine to take care of the $user object
             $em->persist($user); 
@@ -42,11 +44,31 @@ class UserController extends Controller{
             $em->flush();
 
             header('Location: ' . PATH . '/index.php/user/index');
+            return;
         }
 
         return $this->render('user', 'form', [
             "user" => $user
         ]);
+    }
+
+    // Remove a user with id passed in url
+    public function removeAction($args){
+        $em = $this->getDoctrine();
+        $repo = $em->getRepository('\Imie\Entity\User');
+
+        // Find user in repository
+        $user = $repo->find($args[2]);
+
+        // Remove it if it exists
+        if(isset($user)){
+            $em->remove($user);
+            $em->flush();
+        }
+
+        // users list redirection
+        header('Location: ' . PATH . '/index.php/user/index');
+
     }
 
 }
